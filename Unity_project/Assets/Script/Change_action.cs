c334bd1d8b12f6068dabdfb6e7593c9d1202cd53
noahzhy <noahzhy@gmail.com>
2022-02-07 16:06:38 +0900

    add animatorsControllers switch

--- Unity_project/Assets/Script/Change_action.cs
+++ Unity_project/Assets/Script/Change_action.cs
@@ -1,26 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Change_action : MonoBehaviour
 {
     protected Animator animator;
     protected AnimatorOverrideController animatorOverrideController;
-    
+
+    private List<string> animatorsControllers = new List<string>();
+
+    public float action_holding = 5.0f;
+    public float capture_holding = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
+        DirectoryInfo direction = new DirectoryInfo("Assets/Resources/Controller");
+        FileInfo[] files = direction.GetFiles("*.controller");
+        foreach (var file in files) {
+            animatorsControllers.Add("Controller/"+file.Name.Split('.')[0]);
+        }
         animator = this.GetComponent<Animator>();
-        animatorOverrideController = new AnimatorOverrideController(animator.runtimeAnimatorController);
-        animator.runtimeAnimatorController = animatorOverrideController;
         animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load(
-            "BasicMotions@Walk"
+            animatorsControllers[0]
         );
+        InvokeRepeating("changeAC", 0f, action_holding);
+        // // for testing
+        // foreach (var j in animatorsControllers){
+        //     Debug.Log(j);
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void changeAC() {
+        string tmp = animatorsControllers[0];
+        Debug.Log(tmp);
+        animator.runtimeAnimatorController = (RuntimeAnimatorController)Resources.Load(tmp);
+        animatorsControllers.RemoveAt(0);
+        animatorsControllers.Add(tmp);
     }
 }
